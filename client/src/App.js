7a26c8dd96ab97de0eade83525700e7c4d4c9e4b
Zachary Garcia <chubbizg@gmail.com>
2025-01-07 19:46:31 -0500

    added better comments

--- client/src/App.js
+++ client/src/App.js
@@ -7,38 +7,57 @@ function App() {
   const [recipes, setRecipes] = useState([]);
   const [recipeDetails, setRecipeDetails] = useState({});
 
+  //function for searching recipes based on user input ingredients
+  //sends GET request to backend for API interaction
   const searchRecipes = async (ingredients) => {
     if (ingredients.length > 0) {
+
+      //converts array of ingredients into comma-separated string
       const ingredientsQuery = ingredients.join(',');
+
       try {
+        
+        //fetch recipes using ingredients query
         const response = await fetch(`/search-recipes?ingredients=${ingredientsQuery}`);
         const data = await response.json();
 
+        //debug
         console.log(data);
+
+        //updates recipe state with fetched recipes
         setRecipes(data);
+
       } catch (error) {
+        //logs errors for troubleshooting
         console.error("Error fetching recipes:", error);
       }
     }
   };
 
+  //function for getting recipe details by recipe ID
   const fetchRecipeDetails = async (id) => {
-    // Check if we already have the details for this recipe to avoid repeated fetches
+    //check if the recipe details already cached
     if (!recipeDetails[id]) {
       try {
+
+        //fetch recipe details using id query
         const response = await fetch(`/recipe/${id}`);
         const data = await response.json();
+
+        //updates recipeDetails state with fetched details
         setRecipeDetails((prevDetails) => ({
-          ...prevDetails,
-          [id]: data, // Store the details in a dictionary with the recipe ID as the key
+          ...prevDetails, //retain previous details
+          [id]: data, // add or overwrite details for current recipe ID
         }));
       } catch (error) {
+        //log errors for troubleshooting
         console.error("Error fetching recipe details:", error);
       }
     }
   };
 
   return (
+    //wraps application in Router to enable navigation between pages
     <Router>
       <Routes>
         <Route 
