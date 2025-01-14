f77d63113108b9afbb288a88ae503f21c9494b6f
Zachary Garcia <chubbizg@gmail.com>
2025-01-14 15:11:28 -0500

    removed removeIngredient button and added x button to remove specific ingredient

--- client/src/components/Homepage/DiscoverTool/DiscoverTool.js
+++ client/src/components/Homepage/DiscoverTool/DiscoverTool.js
@@ -12,15 +12,13 @@ function DiscoverTool({ onSearch }) {
         }
     };
 
-    const removeIngredient = () => {
-        if(ingredients.length > 0) {
-            setIngredients(ingredients.slice(0,-1));
-        }
-    }
+    const removeSpecificIngredient = (indexToRemove) => {
+        setIngredients(ingredients.filter((_, index) => index !== indexToRemove));
+    };
 
     const searchRecipes = () => {
         onSearch(ingredients);
-    }
+    };
 
     return (
         <div className={`${styles.discoverToolContainer}`}>
@@ -41,18 +39,24 @@ function DiscoverTool({ onSearch }) {
                     <button onClick={addIngredient}>Add Ingredient</button>
                 </div>
             </div>
+
             <div className={`${styles.ingredientsListTab}`}>
                 <div className="Ingredients-List">
                     <ul>
                         {ingredients.map((ing, index) => (
-                        <li key={index}>{ing}</li>
+                        <li key={index}>
+                                {ing}
+                                <button
+                                    className={styles.removeButton}
+                                    onClick={() => removeSpecificIngredient(index)}
+                                >
+                                    x
+                                </button>
+                            </li>
                         ))}
                     </ul>
                 </div>
 
-                <div className={`${styles.removeIngredientButton}`}>
-                    <button onClick={removeIngredient}>Remove Ingredient</button>
-                </div>
                 <div className={`${styles.searchRecipesButton}`}>
                     <button onClick={searchRecipes}>Search Recipes</button>
                 </div>
